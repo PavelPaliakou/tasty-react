bad578994d6aa2b486667bf61bd7409afd6982c4
pavel <pavel.polyakov0010@gmail.com>
2024-05-08 19:21:18 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Button } from './components/ui/button'
+import HomePage from './pages/home-page'
+import RecipePage from './pages/recipe-page'
+import NavBar from './components/ui/nav-bar'
 
 function App() {
 
   return (
     <>
-      
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/recipes/:recipeId" element={<RecipePage/>}/>
+      <Route path="*" element={<p>This page doesn't exist</p>}/>
+    </Routes>
+      <Button>Hi</Button>
     </>
   )
 }
---
+++ src/components/ui/nav-bar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <div className="w-full border-0 py-4 lg:px-24 px-10 bg-gray-950">
+            <h1 className="text-3xl text-white">
+                <a href="/">
+                    Tasty React
+                </a>
+            </h1>
+        </div>
+    )
+}
