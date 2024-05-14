0de51d8d09002452223157b351ebbf7fb0951596
pavel <pavel.polyakov0010@gmail.com>
2024-05-14 23:01:18 +0200

    add recipe page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -10,7 +10,7 @@ import { Badge } from "@/components/ui/badge";
 import { useEffect, useState } from "react";
 import { RecipeType } from "@/types";
 
-function HomePage() {
+export default function HomePage() {
     const [data, setData] = useState<Array<RecipeType>>([]);
     const [filteredRecipes, setFilteredRecipes] = useState<Array<RecipeType>>([]);
     const [badge, setBadge] = useState("");
@@ -18,23 +18,20 @@ function HomePage() {
     const recipes = filteredRecipes.length > 0 ? filteredRecipes : data;
 
     useEffect(() => {
-        const getRecipes = async () => {
+        async function getRecipes() {
             const response = await fetch(`https://dummyjson.com/recipes`);
             const { recipes } = await response.json();
 
             setData(recipes);
-        };
+        }
 
         getRecipes();
     }, []);
 
-    const handleOnBadgeClick = (
-        e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-        cuisine: string
-    ) => {
+    function handleOnBadgeClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>, cuisine: string) {
         e.preventDefault();
         setBadge(cuisine);
-    };
+    }
 
     useEffect(() => {
         if (badge) {
@@ -59,9 +56,9 @@ function HomePage() {
                     "Mediterranean",
                     "Mexican",
                     "Pakistani",
-                ].map((cuisine, idx) => (
+                ].map((cuisine, index) => (
                     <Badge
-                        key={`${cuisine}-${idx}`}
+                        key={`${cuisine}-${index}`}
                         variant="outline"
                         className="border-orange-800 text-gray-900 text-lg mx-2 my-1 hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in duration-200"
                         onClick={(e) => handleOnBadgeClick(e, cuisine)}
@@ -72,13 +69,13 @@ function HomePage() {
             </div>
 
             <div className="grid grid-cols- md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-x-10 gap-y-20 xl:gap-y-32 xl:pt-32 pt-12 pb-40">
-                {recipes.map((item: RecipeType) => (
-                    <a href={`/recipes/${item.id}`} key={item.id}>
+                {recipes.map((recipe: RecipeType) => (
+                    <a href={`/recipes/${recipe.id}`} key={recipe.id}>
                         <Card className="flex flex-col bg-orange-50 hover:scale-105 ease-in duration-200 xl:min-h-[600px] fancyGradient">
                             <CardHeader>
                                 <img
-                                    src={item.image}
-                                    alt={item.name}
+                                    src={recipe.image}
+                                    alt={recipe.name}
                                     width={500}
                                     height={500}
                                     className="bg-cover rounded-md shadow-xl"
@@ -86,21 +83,21 @@ function HomePage() {
                             </CardHeader>
                             <CardContent>
                                 <CardTitle className="uppercase lg:text-3xl relative font-bold line-clamp-2">
-                                    {item.name}
+                                    {recipe.name}
                                 </CardTitle>
                             </CardContent>
                             <CardFooter className="flex items-start gap-2 lg:gap-12 lg:flex-row flex-col">
                                 <div className="flex flex-col">
                                     <p className="text-lg">Serves</p>
-                                    <p className="text-gray-800">{item.servings}</p>
+                                    <p className="text-gray-800">{recipe.servings}</p>
                                 </div>
                                 <div className="flex flex-col">
                                     <p className="text-lg">Prep Time</p>
-                                    <p className="text-gray-800">{item.prepTimeMinutes} MIN</p>
+                                    <p className="text-gray-800">{recipe.prepTimeMinutes} MIN</p>
                                 </div>
                                 <div className="flex flex-col">
                                     <p className="text-lg">Cook Time</p>
-                                    <p className="text-gray-800">{item.cookTimeMinutes} MIN</p>
+                                    <p className="text-gray-800">{recipe.cookTimeMinutes} MIN</p>
                                 </div>
                             </CardFooter>
                         </Card>
@@ -110,5 +107,3 @@ function HomePage() {
         </div>
     );
 }
-
-export default HomePage;
